b707276863fb978d6e538e3e51da0d830468d5a5
Steph Hoel <steph.hoel@gmail.com>
2021-12-14 11:23:22 -0300

    Add Conversão de Dólar da Aula 02

---
+++ aula02ConversaoDolar/aula02ConversaoDolar/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace aula02ConversaoDolar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe o valor em dólares:";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(21, 49);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(232, 31);
+            this.txtValue.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(76, 203);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(112, 34);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Converter";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtTax
+            // 
+            this.txtTax.Location = new System.Drawing.Point(21, 143);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.Size = new System.Drawing.Size(232, 31);
+            this.txtTax.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Informe a taxa de câmbio atual:";
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(21, 312);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(232, 31);
+            this.txtRes.TabIndex = 6;
+            this.txtRes.Text = "R$ 000,00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Valor Convertido:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 444);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Conversão";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtTax;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRes;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ aula02ConversaoDolar/aula02ConversaoDolar/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula02ConversaoDolar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtRes.Text = "Informe os valores primeiro";
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double value, tax, res;
+
+            value = Convert.ToDouble(txtValue.Text);
+            tax   = Convert.ToDouble(txtTax.Text);
+
+            res = value * tax;
+
+            txtRes.Text = $"R$ {res.ToString("0.00")}";
+        }
+    }
+}
