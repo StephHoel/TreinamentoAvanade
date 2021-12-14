18c49fc6795366766e87287f7f1cd5b2f54f5c00
Steph Hoel <steph.hoel@gmail.com>
2021-12-14 15:25:49 -0300

    Add If Composto na Aula 02

---
+++ aula02IfComposto/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace aula02IfComposto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor da Doação:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(165, 9);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(193, 31);
+            this.txtValor.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(12, 61);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(346, 34);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular Investimento";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 126);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ aula02IfComposto/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula02IfComposto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double doacao, investir;
+
+            doacao = double.Parse(txtValor.Text.Replace(",","."));
+
+            if (doacao <= 1000)
+                investir = doacao * 0.05;
+            else
+                investir = doacao * 0.15;
+
+            MessageBox.Show($"O valor do investimento será de R$ " +
+                investir.ToString("0.00").Replace(".",","), "Investimento",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
