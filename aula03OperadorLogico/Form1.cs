8aca5f74348e4d8dc340180aabde19a5f181697b
Steph Hoel <steph.hoel@gmail.com>
2021-12-15 11:22:01 -0300

    Add Operador Lógico na Aula 03

---
+++ aula03OperadorLogico/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula03OperadorLogico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string usuario, senha;
+
+            usuario = txtUsuario.Text.ToUpper();
+            senha = txtSenha.Text.ToUpper();
+
+            if (usuario == "ADMINISTRADOR" && senha == "123")
+            {
+                MessageBox.Show("Bem vindo " + usuario);
+            }
+            else
+            {
+                MessageBox.Show("Usuário e/ou Senha incorreta");
+            }
+        }
+    }
+}
