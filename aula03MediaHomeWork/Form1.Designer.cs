fbe510f4882561acd852b0ec2a8b98933d85dc01
Steph Hoel <steph.hoel@gmail.com>
2021-12-16 10:08:14 -0300

    Add Exercício 02 (Média e Situação) na Aula 03

---
+++ aula03MediaHomeWork/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace aula03MediaHomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAssistida = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota 1:";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(12, 41);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(193, 31);
+            this.txtN1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota 2:";
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(235, 41);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(193, 31);
+            this.txtN2.TabIndex = 2;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(12, 170);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(416, 34);
+            this.btnVerificar.TabIndex = 5;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(235, 117);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(193, 31);
+            this.txtTotal.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Aulas Totais:";
+            // 
+            // txtAssistida
+            // 
+            this.txtAssistida.Location = new System.Drawing.Point(12, 117);
+            this.txtAssistida.Name = "txtAssistida";
+            this.txtAssistida.Size = new System.Drawing.Size(193, 31);
+            this.txtAssistida.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Aulas Assistidas:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 219);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAssistida);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAssistida;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ aula03MediaHomeWork/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula03MediaHomeWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double n1, n2, media, frequencia,assistida, total;
+            string situacao;
+
+            //Elabore um programa que leia 2 notas escolares. Calcule a média
+            n1 = double.Parse(txtN1.Text);
+            n2 = double.Parse(txtN2.Text);
+            media = (n1 + n2) / 2;
+
+            //Leia também a quantidade de aulas dadas e a quantidade de aulas assistidas
+            //pelo aluno e calcule sua frequencia :
+            //(aulasAssistidas / aulasDadas) * 100
+            assistida = double.Parse(txtAssistida.Text);
+            total = double.Parse(txtTotal.Text);
+            frequencia = (assistida / total) * 100;
+
+            //O aluno somente estará aprovado com média maior ou igual a 7 e 
+            //frequencia mínima de 75 %
+            if (media >= 7 && frequencia >= 75)
+                situacao = "Aprovado";
+            else
+                situacao = "Reprovado";
+
+            //Exiba a média, a frequência e a mensagem informando sua situação
+            MessageBox.Show($"Média: {media.ToString("0.00")}\n" +
+                $"Frequência: {frequencia.ToString("0.00")}%\n" +
+                $"Situação: {situacao}", "Resultado");
+
+        }
+    }
+}
