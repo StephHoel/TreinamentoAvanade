7cf6160bd41534fa4c695aef989dc162f874d99b
Steph Hoel <steph.hoel@gmail.com>
2021-12-16 09:28:47 -0300

    Add Exercício 01 (Banco) na Aula 03

---
+++ aula03HomeWork/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace aula03HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.cmbTransacao = new System.Windows.Forms.ComboBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe a transação:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(12, 118);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(280, 31);
+            this.txtValor.TabIndex = 2;
+            // 
+            // cmbTransacao
+            // 
+            this.cmbTransacao.FormattingEnabled = true;
+            this.cmbTransacao.Items.AddRange(new object[] {
+            "",
+            "Depósito",
+            "Saque",
+            "Ver Saldo"});
+            this.cmbTransacao.Location = new System.Drawing.Point(13, 41);
+            this.cmbTransacao.Name = "cmbTransacao";
+            this.cmbTransacao.Size = new System.Drawing.Size(280, 33);
+            this.cmbTransacao.TabIndex = 1;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(13, 179);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(280, 34);
+            this.btnConfirmar.TabIndex = 3;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Informe o valor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 250);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.cmbTransacao);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.ComboBox cmbTransacao;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ aula03HomeWork/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace aula03HomeWork
+{
+    public partial class Form1 : Form
+    {
+        double saldo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            int menu = cmbTransacao.SelectedIndex;
+
+            if (!(double.TryParse(txtValor.Text, out double valor)) && (valor <= 0 && menu != 3))
+            {
+                MessageBox.Show("Valor informado incorretamente!", "Erro");
+                txtValor.Clear();
+                txtValor.Focus();
+                return;
+            }
+
+
+            switch (menu)
+            {
+                case 1: // Depósito
+                    saldo += valor;
+                    MessageBox.Show($"Depósito de R$ {valor.ToString("0.00")} realizado com sucesso!", "Depósito");
+                    break;
+                case 2: // Saque
+                    saldo -= valor;
+                    MessageBox.Show($"Saque de R$ {valor.ToString("0.00")} realizado com sucesso!", "Saque");
+                    break;
+                case 3: // Ver Saldo
+                    MessageBox.Show($"Saldo de R$ {saldo.ToString("0.00")} em conta.", "Saldo");
+                    break;
+                default:
+                    MessageBox.Show("Operação inválida, tente novamente!", "Erro");
+                    break;
+            }
+        }
+    }
+}
