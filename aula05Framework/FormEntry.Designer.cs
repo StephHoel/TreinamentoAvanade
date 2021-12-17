f1b4e915e32a821a60907e3f80a305bbd28a6d4d
Steph Hoel <steph.hoel@gmail.com>
2021-12-17 11:17:19 -0300

    Add Entry na Aula 05 Framework

---
+++ aula05Framework/FormEntry.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace aula05Framework
+{
+    partial class FormEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArrayInterative = new System.Windows.Forms.Button();
+            this.BtnArrayInverse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArrayInterative
+            // 
+            this.btnArrayInterative.Location = new System.Drawing.Point(13, 13);
+            this.btnArrayInterative.Name = "btnArrayInterative";
+            this.btnArrayInterative.Size = new System.Drawing.Size(232, 53);
+            this.btnArrayInterative.TabIndex = 0;
+            this.btnArrayInterative.Text = "Array Interative";
+            this.btnArrayInterative.UseVisualStyleBackColor = true;
+            this.btnArrayInterative.Click += new System.EventHandler(this.btnArrayInterative_Click);
+            // 
+            // BtnArrayInverse
+            // 
+            this.BtnArrayInverse.Location = new System.Drawing.Point(13, 85);
+            this.BtnArrayInverse.Name = "BtnArrayInverse";
+            this.BtnArrayInverse.Size = new System.Drawing.Size(232, 53);
+            this.BtnArrayInverse.TabIndex = 1;
+            this.BtnArrayInverse.Text = "Array Inverse";
+            this.BtnArrayInverse.UseVisualStyleBackColor = true;
+            this.BtnArrayInverse.Click += new System.EventHandler(this.BtnArrayInverse_Click);
+            // 
+            // FormEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 450);
+            this.Controls.Add(this.BtnArrayInverse);
+            this.Controls.Add(this.btnArrayInterative);
+            this.Name = "FormEntry";
+            this.Text = "Entrada";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArrayInterative;
+        private System.Windows.Forms.Button BtnArrayInverse;
+    }
+}
---
+++ aula05Framework/FormEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula05Framework
+{
+    public partial class FormEntry : Form
+    {
+        public FormEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArrayInterative_Click(object sender, EventArgs e)
+        {
+            FormArrayInterative formArrayInterative = new FormArrayInterative();
+            formArrayInterative.Show();
+        }
+
+        private void BtnArrayInverse_Click(object sender, EventArgs e)
+        {
+            FormVetorInverso formVetorInverso = new FormVetorInverso();
+            formVetorInverso.Show();
+        }
+    }
+}
