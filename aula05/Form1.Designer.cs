9e2ff840d05baf092434e5afda44af7163ca0ce9
Steph Hoel <steph.hoel@gmail.com>
2021-12-17 09:59:32 -0300

    Add Aula 05

---
+++ aula05/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace aula05
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnArray = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnArray
+            // 
+            this.BtnArray.Location = new System.Drawing.Point(13, 13);
+            this.BtnArray.Name = "BtnArray";
+            this.BtnArray.Size = new System.Drawing.Size(266, 34);
+            this.BtnArray.TabIndex = 0;
+            this.BtnArray.Text = "Exercício Array";
+            this.BtnArray.UseVisualStyleBackColor = true;
+            this.BtnArray.Click += new System.EventHandler(this.BtnArray_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 450);
+            this.Controls.Add(this.BtnArray);
+            this.Name = "Form1";
+            this.Text = "Aula 05";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnArray;
+    }
+}
+
