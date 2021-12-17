9d55948d6c722660d647ee10a417f89fc195e698
Steph Hoel <steph.hoel@gmail.com>
2021-12-17 10:50:27 -0300

    Add Aula 05 Framework em Aula 05

---
+++ aula05Framework/FormArrayInterative.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace aula05Framework
+{
+    partial class FormArrayInterative
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnIniciar
+            // 
+            this.BtnIniciar.Location = new System.Drawing.Point(63, 23);
+            this.BtnIniciar.Name = "BtnIniciar";
+            this.BtnIniciar.Size = new System.Drawing.Size(181, 47);
+            this.BtnIniciar.TabIndex = 0;
+            this.BtnIniciar.Text = "Iniciar";
+            this.BtnIniciar.UseVisualStyleBackColor = true;
+            this.BtnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 102);
+            this.Controls.Add(this.BtnIniciar);
+            this.Name = "Form1";
+            this.Text = "Aula 05";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnIniciar;
+    }
+}
+
---
+++ aula05Framework/FormArrayInterative.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace aula05Framework
+{
+    public partial class FormArrayInterative : Form
+    {
+        public FormArrayInterative()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            double[] v = new double[5];
+            double total = 0;
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = double.Parse(Interaction.InputBox($"Digite o valor da venda do vendedor {i + 1}:"));
+                total += v[i];
+            }
+
+            MessageBox.Show($"Total das vendas R$ {total.ToString("0.00")}");
+
+        }
+    }
+}
